d200d18da1a61179b6a8182bead8fac01162bcb3
rahulrana-dev <rahulrana4504@gmail.com>
2025-07-18 07:39:00 +0530

    initial setup complete

---
+++ notenest/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './Components/Header'
+import Notes from './Components/Notes'
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+      <Header />
+      <Notes />
+
+    </div>
+    
+  )
+}
+
+export default App
